ca15d1321fb385f09982fe8d9d5adeda1be01ab5
Pierre Lannoy <pierre@lannoy.fr>
2021-11-03 11:31:46 +0100

    Initial commit

---
+++ includes/plugin/class-updater.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Plugin updates handling.
+ *
+ * @package Plugin
+ * @author  Pierre Lannoy <https://pierre.lannoy.fr/>.
+ * @since   1.0.0
+ */
+
+namespace WPPluginBoilerplate\Plugin;
+
+use WPPluginBoilerplate\System\Nag;
+use WPPluginBoilerplate\System\Option;
+use WPPluginBoilerplate\System\Role;
+use WPPluginBoilerplate\System\Logger;
+use WPPluginBoilerplate\System\Environment;
+use Exception;
+use WPPluginBoilerplate\System\Markdown;
+
+/**
+ * Plugin updates handling.
+ *
+ * This class defines all code necessary to handle the plugin's updates.
+ *
+ * @package Plugin
+ * @author  Pierre Lannoy <https://pierre.lannoy.fr/>.
+ * @since   1.0.0
+ */
+class Updater {
+
+	/**
+	 * Initializes the class, set its properties and performs
+	 * post-update processes if needed.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		$old = Option::network_get( 'version' );
+		Option::network_set( 'version', TRAFFIC_VERSION );
+		if ( TRAFFIC_VERSION !== $old ) {
+			if ( '0.0.0' === $old ) {
+				$this->install();
+				// phpcs:ignore
+				$message = sprintf( esc_html__( '%1$s has been correctly installed.', 'wp-plugin-boilerplate' ), WPPB_PRODUCT_NAME );
+			} else {
+				$this->update( $old );
+				// phpcs:ignore
+				$message  = sprintf( esc_html__( '%1$s has been correctly updated from version %2$s to version %3$s.', 'wp-plugin-boilerplate' ), WPPB_PRODUCT_NAME, $old, WPPB_VERSION );
+				Logger::notice( $message );
+				// phpcs:ignore
+				$message .= ' ' . sprintf( __( 'See <a href="%s">what\'s new</a>.', 'wp-plugin-boilerplate' ), admin_url( 'options-general.php?page=wp-plugin-boilerplate-settings&tab=about' ) );
+			}
+			Nag::add( 'update', 'info', $message );
+		}
+	}
+
+	/**
+	 * Performs post-installation processes.
+	 *
+	 * @since 1.0.0
+	 */
+	private function install() {
+
+	}
+
+	/**
+	 * Performs post-update processes.
+	 *
+	 * @param   string $from   The version from which the plugin is updated.
+	 * @since 1.0.0
+	 */
+	private function update( $from ) {
+
+	}
+
+	/**
+	 * Is the WP update system enabled for plugins?
+	 *
+	 * @return  boolean  True if nothing blocks updates, false otherwise.
+	 * @since 1.0.0
+	 */
+	public function is_updatable() {
+		$filter = false === has_filter( 'auto_update_plugin', '__return_false' );
+		if ( defined( 'AUTOMATIC_UPDATER_DISABLED' ) ) {
+			$main = ! AUTOMATIC_UPDATER_DISABLED;
+		} else {
+			$main = true;
+		}
+		return $main && $filter;
+	}
+
+	/**
+	 * Is the plugin auto-update enabled?
+	 *
+	 * @return  boolean  True if plugin is auto-updatable, false otherwise.
+	 * @since 1.0.0
+	 */
+	public function is_autoupdatable() {
+		return ( $this->is_updatable() && Option::site_get( 'auto_update' ) );
+	}
+
+	/**
+	 * Choose if the plugin must be auto-updated or not.
+	 * Concerned hook: auto_update_plugin.
+	 *
+	 * @param   boolean $update The default answer.
+	 * @param   object  $item   The detail of the item (to update or not).
+	 * @return  boolean  True if plugin must be auto-updated, false otherwise.
+	 * @since 1.0.0
+	 */
+	public function auto_update_plugin( $update, $item ) {
+		if ( ( WPPB_SLUG === $item->slug ) && $this->is_autoupdatable() ) {
+			return true;
+		} else {
+			return $update;
+		}
+	}
+
+	/**
+	 * Get the changelog.
+	 *
+	 * @param   array $attributes  'style' => 'markdown', 'html'.
+	 *                             'mode'  => 'raw', 'clean'.
+	 * @return  string  The output of the shortcode, ready to print.
+	 * @since 1.0.0
+	 */
+	public function sc_get_changelog( $attributes ) {
+		$md = new Markdown();
+		return $md->get_shortcode(  'CHANGELOG.md', $attributes  );
+	}
+}
---
+++ includes/system/class-role.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Roles handling
+ *
+ * Handles all roles operations and detection.
+ *
+ * @package System
+ * @author  Pierre Lannoy <https://pierre.lannoy.fr/>.
+ * @since   1.0.0
+ */
+
+namespace WPPluginBoilerplate\System;
+
+/**
+ * Define the roles functionality.
+ *
+ * Handles all roles operations and detection.
+ *
+ * @package System
+ * @author  Pierre Lannoy <https://pierre.lannoy.fr/>.
+ * @since   1.0.0
+ */
+class Role {
+
+	/**
+	 * The super (network) admin.
+	 *
+	 * @since  1.0.0
+	 */
+	const SUPER_ADMIN = 4;
+
+	/**
+	 * The single site admin.
+	 *
+	 * @since  1.0.0
+	 */
+	const SINGLE_ADMIN = 2;
+
+	/**
+	 * The local admin (in network site).
+	 *
+	 * @since  1.0.0
+	 */
+	const LOCAL_ADMIN = 1;
+
+	/**
+	 * Initializes the class and set its properties.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+	}
+
+	/**
+	 * Get a user nice name.
+	 *
+	 * @param   integer $user_id         Optional. The user id.
+	 * @return  integer  The type of admin.
+	 * @since   1.0.0
+	 */
+	public static function admin_type( $user_id = false ) {
+		if ( ! $user_id ) {
+			$user = wp_get_current_user();
+		} else {
+			$user = get_userdata( $user_id );
+		}
+		if ( ! $user || ! $user->exists() ) {
+			return 0;
+		}
+		if ( is_multisite() ) {
+			$super_admins = get_super_admins();
+			if ( is_array( $super_admins ) && in_array( $user->user_login, $super_admins ) ) {
+				return self::SUPER_ADMIN;
+			} elseif ( in_array( 'administrator', $user->roles ) ) {
+				return self::LOCAL_ADMIN;
+			}
+		} else {
+			if ( in_array( 'administrator', $user->roles ) ) {
+				return self::SINGLE_ADMIN;
+			}
+		}
+		return 0;
+	}
+
+	/**
+	 * Get a list of available roles.
+	 *
+	 * @return  array  The list of available roles.
+	 * @since   1.0.0
+	 */
+	public static function get_all() {
+		global $wp_roles;
+		if ( ! isset( $wp_roles ) ) {
+			$wp_roles = new \WP_Roles();
+		}
+		$result = [];
+		foreach ( $wp_roles->get_names() as $role => $name ) {
+			$result[ $role ]              = [];
+			$result[ $role ]['name']      = $name;
+			$result[ $role ]['l10n_name'] = translate_user_role( $name );
+		}
+		return $result;
+	}
+
+	/**
+	 * Get user main role.
+	 *
+	 * @param   integer $user_id   The user id.
+	 * @return  string  The role id.
+	 * @since    1.0.0
+	 */
+	public static function get_user_main( $user_id ) {
+		$user = get_user_by( 'id', $user_id );
+		$role = '';
+		foreach ( self::get_all() as $key => $detail ) {
+			if ( in_array( $key, $user->roles, true ) ) {
+				$role = $key;
+				break;
+			}
+		}
+		return $role;
+	}
+
+	/**
+	 * Get all user roles.
+	 *
+	 * @param   integer $user_id   The user id.
+	 * @return  array  The roles id.
+	 * @since    1.0.0
+	 */
+	public static function get_user_all( $user_id ) {
+		$user = get_user_by( 'id', $user_id );
+		$role = [];
+		foreach ( self::get_all() as $key => $detail ) {
+			if ( in_array( $key, $user->roles, true ) ) {
+				$role[] = $key;
+				break;
+			}
+		}
+		return $role;
+	}
+
+}
