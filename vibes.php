fe19ccd6c278a718687e45cea9c356ced02195d9
Pierre Lannoy <pierre@hosterra.eu>
2024-05-04 18:35:06 +0200

    Ready to release 1.8.1 on wp.org

--- vibes.php
+++ vibes.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Vibes
  * Plugin URI:        https://perfops.one/vibes
  * Description:       Truthful user experience and browsing performances monitoring.
- * Version:           1.8.0
+ * Version:           1.8.1
  * Requires at least: 6.2
  * Requires PHP:      8.1
  * Author:            Pierre Lannoy / PerfOps One
