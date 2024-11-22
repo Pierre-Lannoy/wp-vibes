c667585df3c867fc1b8f8ba4989cecec4487cc5b
Pierre Lannoy <pierre@hosterra.eu>
2024-11-22 10:03:16 +0100

    Uses object cache first, then transient…

--- includes/plugin/class-updater.php
+++ includes/plugin/class-updater.php
@@ -12,6 +12,7 @@ namespace Vibes\Plugin;
 use Vibes\Plugin\Feature\Schema;
 use Vibes\System\Nag;
 use Vibes\System\Option;
+use Vibes\System\Cache;
 use Vibes\System\Environment;
 
 use Vibes\System\Role;
@@ -112,7 +113,7 @@ class Updater {
 	 * @return  object   The remote info.
 	 */
 	private function gather_info() {
-		$remotes = get_transient( 'update-' . $this->slug );
+		$remotes = Cache::get_global( 'data_update-infos' );
 		if ( ! $remotes ) {
 			$remotes = new \stdClass();
 
@@ -136,7 +137,7 @@ class Updater {
 			$remotes->author_profile = $plugin_info['author_profile'] ?? 'https://profiles.wordpress.org/pierrelannoy/';
 
 			$remote = wp_remote_get(
-				str_replace( 'github.com', 'api.github.com/repos', $this->product ) . '/releases/latest',
+				'https://releases.perfops.one/' . $this->slug . '.json',
 				[
 					'timeout' => 10,
 					'headers' => [
@@ -160,7 +161,7 @@ class Updater {
 				return false;
 			}
 
-			set_transient( 'update-' . $this->slug, $remotes, DAY_IN_SECONDS );
+			Cache::set_global( 'data_update-infos', $remotes, DAY_IN_SECONDS );
 		}
 
 		return $remotes;
